7888b0b2a4d713ec16ffbe78e9df80542afe7c0a
Matthias Hannen <matthias.hannen@gmail.com>
2022-12-14 22:35:51 +0100

    Detect dispenser item

--- HUB/Warehouse/Dispenser.lua
+++ HUB/Warehouse/Dispenser.lua
@@ -6,42 +6,43 @@ Colors = {
     error = { r = 17, g = 0, b = 0, a = 0.01 }
 }
 
-StorageItems = {
+WarehouseItems = {
+    None = { item = nil, icon = 341 },
     -- Infrastructure
-    I1 = { item = findItem('Cable'), icon = 208 },
-    I2 = { item = findItem('Circuit Board'), icon = 243 },
-    I3 = { item = findItem('Computer'), icon = 271 },
-    I4 = { item = findItem('Concrete'), icon = 211 },
-    I5 = { item = findItem('Copper Sheet'), icon = 220 },
-    I6 = { item = findItem('Encased Industrial Beam'), icon = 221 },
-    I7 = { item = findItem('Heavy Modular Frame'), icon = 225 },
-    I8 = { item = findItem('Iron Plate'), icon = 195 },
-    I9 = { item = findItem('Iron Rod'), icon = 196 },
-    I10 = { item = findItem('Motor'), icon = 223 },
-    I11 = { item = findItem('Plastic'), icon = 224 },
-    I12 = { item = findItem('Quartz Crystal'), icon = 218 },
-    I13 = { item = findItem('Reinforced Iron Plate'), icon = 207 },
-    I14 = { item = findItem('Rotor'), icon = 232 },
-    I15 = { item = findItem('Silica'), icon = 275 },
-    I16 = { item = findItem('Steel Beam'), icon = 219 },
-    I17 = { item = findItem('Steel Pipe'), icon = 256 },
-    I18 = { item = findItem('Wire'), icon = 209 },
+    Cable = { item = findItem('Cable'), icon = 208 },
+    CircuitBoard = { item = findItem('Circuit Board'), icon = 243 },
+    Computer = { item = findItem('Computer'), icon = 271 },
+    Concrete = { item = findItem('Concrete'), icon = 211 },
+    CopperSheet = { item = findItem('Copper Sheet'), icon = 220 },
+    EncasedIndustrialBeam = { item = findItem('Encased Industrial Beam'), icon = 221 },
+    HeavyModularFrame = { item = findItem('Heavy Modular Frame'), icon = 225 },
+    IronPlate = { item = findItem('Iron Plate'), icon = 195 },
+    IronRod = { item = findItem('Iron Rod'), icon = 196 },
+    Motor = { item = findItem('Motor'), icon = 223 },
+    Plastic = { item = findItem('Plastic'), icon = 224 },
+    QuartzCrystal = { item = findItem('Quartz Crystal'), icon = 218 },
+    ReinforcedIronPlate = { item = findItem('Reinforced Iron Plate'), icon = 207 },
+    Rotor = { item = findItem('Rotor'), icon = 232 },
+    Silica = { item = findItem('Silica'), icon = 275 },
+    SteelBeam = { item = findItem('Steel Beam'), icon = 219 },
+    SteelPipe = { item = findItem('Steel Pipe'), icon = 256 },
+    Wire = { item = findItem('Wire'), icon = 209 },
     -- Facilities
-    F1 = { item = findItem('AI Limiter'), icon = 230 },
-    F2 = { item = findItem('Alclad Aluminium Sheet'), icon = 227 },
-    F3 = { item = findItem('Aluminium Casing'), icon = 228 },
-    F4 = { item = findItem('Cooling System'), icon = 259 },
-    F5 = { item = findItem('Crystal Oscillator'), icon = 270 },
-    F6 = { item = findItem('Electromagnetic Control Rod'), icon = 260 },
-    F7 = { item = findItem('Fused Modular Frame'), icon = 265 },
-    F8 = { item = findItem('High-Speed Connector'), icon = 226 },
-    F9 = { item = findItem('Modular Frame'), icon = 233 },
-    F10 = { item = findItem('Quickwire'), icon = 274 },
-    F11 = { item = findItem('Radio Control Unit'), icon = 229 },
-    F12 = { item = findItem('Rubber'), icon = 222 },
-    F13 = { item = findItem('Stator'), icon = 247 },
-    F14 = { item = findItem('Supercomputer'), icon = 267 },
-    F15 = { item = findItem('Turbo Motor'), icon = 273 },
+    AILimiter = { item = findItem('AI Limiter'), icon = 230 },
+    AlcladAluminiumSheet = { item = findItem('Alclad Aluminium Sheet'), icon = 227 },
+    AluminiumCasing = { item = findItem('Aluminium Casing'), icon = 228 },
+    CoolingSystem = { item = findItem('Cooling System'), icon = 259 },
+    CrystalOscillator = { item = findItem('Crystal Oscillator'), icon = 270 },
+    ElectromagneticControlRod = { item = findItem('Electromagnetic Control Rod'), icon = 260 },
+    FusedModularFrame = { item = findItem('Fused Modular Frame'), icon = 265 },
+    HighSpeedConnector = { item = findItem('High-Speed Connector'), icon = 226 },
+    ModularFrame = { item = findItem('Modular Frame'), icon = 233 },
+    Quickwire = { item = findItem('Quickwire'), icon = 274 },
+    RadioControlUnit = { item = findItem('Radio Control Unit'), icon = 229 },
+    Rubber = { item = findItem('Rubber'), icon = 222 },
+    Stator = { item = findItem('Stator'), icon = 247 },
+    Supercomputer = { item = findItem('Supercomputer'), icon = 267 },
+    TurboMotor = { item = findItem('Turbo Motor'), icon = 273 }
 }
 
 function GetSign(name)
@@ -85,51 +86,78 @@ function GetColor(level)
     return Colors.ok
 end
 
-function ContainerLevel(container, storageItem)
-    if storageItem then
+function ContainerLevel(container, warehouseItem)
+    local level = 0
+
+    if warehouseItem then
         local inventory = container:getInventories()[1]
+        local max = inventory.size * warehouseItem['item'].max
         local count = 0
-        local max = inventory.size * storageItem['item'].max
 
         for i = 0, inventory.size - 1, 1 do
             local stack = inventory:getStack(i)
             count = count + stack.count
         end
 
-        return count / max
-    else
-        return 0
+        level = count / max
     end
+
+    return level
 end
 
-function UpdateLevelDisplay(container, storageItem)
+function UpdateLevelDisplay(container, warehouseItem)
     local displayComps = component.findComponent("LevelDisplays")
-    local color = GetColor(ContainerLevel(container, storageItem))
+    local color = GetColor(ContainerLevel(container, warehouseItem))
 
     for _, display in ipairs(GetSigns('LevelDisplays')) do
         SetSignColor(display, color)
     end
 end
 
-function AssignItem(container, storageItem)
-    if storageItem then
+function AssignItem(container, warehouseItem)
+    if warehouseItem then
         local signComps = component.findComponent("Signs")
 
         for _, sign in ipairs(GetSigns('Signs')) do
-            SetSignIcon(sign, storageItem['icon'])
-            -- TODO Set splitter
+            SetSignIcon(sign, warehouseItem['icon'])
         end
     else
         print('Unknown item ' .. container.nick)
     end
 end
 
-while true do
-    local container = component.proxy(component.findComponent(findClass("FGBuildableStorage"))[1])
-    local storageItem = StorageItems[container.nick]
+function GetItem(container)
+    local item = WarehouseItems.None
 
-    AssignItem(container, storageItem)
-    UpdateLevelDisplay(container, storageItem)
+    for _, c in pairs(container) do
+        local inventory = container:getInventories()[1]
+        local stack = inventory:getStack(0)
+
+        if stack.count > 0 then
+            local itemId = string.gsub(stack.item.name, '( |-)', '')
+            return WarehouseItems[itemId]
+        end
+    end
+
+    return item
+end
+
+function GetContainer()
+    local container = {}
+
+    for _, c in pairs(component.findComponent(findClass("FGBuildableStorage"))) do
+        table.insert(container, component.proxy(c))
+    end
+
+    return container
+end
+
+while true do
+    local container = GetContainer()
+    local warehouseItem = GetItem(container)
+
+    AssignItem(container, warehouseItem)
+    UpdateLevelDisplay(container, warehouseItem)
 
     event.pull(RefreshInterval)
 end
