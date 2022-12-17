6d018b161a65fe71dc79fa59bf05117cdab768e6
Matthias Hannen <matthias.hannen@gmail.com>
2022-12-17 17:00:58 +0100

    Fix network init

--- Libs/Network.lua
+++ Libs/Network.lua
@@ -5,9 +5,11 @@ Networks = {
     HUB = {
         id = 'HUB',
         port = 2,
-        ControlCenter = { id = 'ControlCenter', group = Networks.HUB, port = 21 },
-        ProjectAssembly = { id = 'ProjectAssembly', group = Networks.HUB, port = 22 },
-        Warehouse = { id = 'Warehouse', group = Networks.HUB, port = 23 }
+        subnets = {
+            ControlCenter = { id = 'ControlCenter', group = 'HUB', port = 21 },
+            ProjectAssembly = { id = 'ProjectAssembly', group = 'HUB', port = 22 },
+            Warehouse = { id = 'Warehouse', group = 'HUB', port = 23 }
+        }
     },
     Highway = {
         id = 'Highway',
@@ -16,35 +18,82 @@ Networks = {
     Power = {
         id = 'Power',
         port = 4,
-        CoalPlant = { id = 'CoalPlant', group = Networks.Power, port = 41 }
+        subnets = {
+            CoalPlant = { id = 'CoalPlant', group = 'Power', port = 41 }
+        }
     }
 }
 
-Network.Commands = {
-    Update = 'Update'
+Network = {
+    device = component.proxy(component.findComponent(findClass("NetworkCard"))[1]),
+    id = nil,
+    port = nil,
+    subnets = {},
+    group = nil,
+    commands = {
+        Update = 'Update'
+    }
 }
 
-
 function Network:init()
-    for _, net in ipairs(Networks) do
-        if net.id == self.device.nick then
-            return net
-        end
+    if not self.device then
+        computer.panic('No network card found!')
     end
 
-    computer.panic('Cannot find network ' .. self.device.nick .. '!')
-
+    self:setNetwork()
     event.listen(self.device)
     self:openPorts()
+end
+
+function Network:setNetwork()
+    local network = self:findNetwork(Networks)
+    if not network then
+        computer.panic('Cannot find network ' .. self.device.nick .. '!')
+    end
+
+    self.id = network.id
+    self.port = network.port
+    self.subnets = network.subnets
+    self.group = network.group
+end
+
+function Network:findNetwork(nets)
+    for _, net in pairs(nets) do
+        local network = nil
+        if net.id == self.device.nick then
+            network = net
+        elseif net.subnets then
+            network = self:findNetwork(net.subnets)
+        end
+        if network then
+            return network
+        end
+    end
+end
+
+function Network:openPorts()
+    self.device:open(self.port)
+
+    if self.group then
+        self:openPort(Networks[self.group])
+    end
+end
+
+function Network:openPort(network)
+    self.device:open(network.port)
+
+    if network.group then
+        self:openPort(Networks[network.group])
+    end
 end
 
 function Network:isNetwork(port)
     if port == Network.ALL.port then
         return true
-    elseif port == self.current.port then
+    elseif port == self.port then
         return true
     elseif self.group then
-        return self.group:isNetwork(port)
+        return Networks[self.group]:isNetwork(port)
     else
         return false
     end
@@ -82,16 +131,4 @@ function Network:send(d1, d2, d3, d4, d5, d6, d7)
     self.device.broadcast(self.port, d1, d2, d3, d4, d5, d6, d7)
 end
 
-function Network:openPorts()
-    self.device:open(self.port)
-
-    if self.group then
-        self.group:openPorts()
-    end
-end
-
-Network.device = component.proxy(component.findComponent(findClass("NetworkCard")[1]))
-if not Network.device then
-    computer.panic('No network card found!')
-end
-Network = Network:init()
+Network:init()
